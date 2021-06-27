66f4882f2af9fa183ec678bf667c4f15804e3d41
Mads Hougesen <madshougesen@gmail.com>
2021-06-27 19:02:38 +0200

    version 1

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import RandomColor from './RandomColor';
+export * from './RandomColor';
+export default RandomColor;
+
+/* const rbg = RandomColor.rgb({ b: { min: 0.5, max: 0.8 } });
+const rbga = RandomColor.rgba({
+  a: { min: 0.5, max: 0.8 },
+});
+
+console.log(rbg, rbga); */
